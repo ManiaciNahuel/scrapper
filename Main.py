687a01e4492ca7bad5404206b0a554aef5dcf4ea
ManiaciNahuel <nahuelmaniaci@gmail.com>
2024-05-18 11:56:10 -0300

    Super mami agregado

--- Main.py
+++ Main.py
@@ -2,13 +2,14 @@ import pandas as pd
 from Carrefour import buscador_carrefour
 from Libertad import buscador_libertad
 from Lides import scrape_farmacia_lider
+from SuperMami import buscador_superMami
 
 def leer_codigos_desde_excel(archivo_excel):
     try:
-            # Lee el archivo Excel y obtiene los códigos de barras de la primera columna
-            df = pd.read_excel(archivo_excel, header=None)  # Sin encabezados
-            codigos = df.iloc[:, 0].tolist()  # Toma solo la primera columna
-            return codigos
+        # Lee el archivo Excel y obtiene los códigos de barras de la primera columna
+        df = pd.read_excel(archivo_excel, header=None)  # Sin encabezados
+        codigos = df.iloc[:, 0].tolist()  # Toma solo la primera columna
+        return codigos
     except Exception as e:
         print("Error al leer el archivo Excel:", e)
         return []
@@ -17,6 +18,7 @@ archivo_excel = "codigos.xlsx"  # Ruta de tu archivo Excel
 codigos_barra = leer_codigos_desde_excel(archivo_excel)
 
 for codigo in codigos_barra:
+    print("\n")
     print(f"Buscando productos para el código de barras: {codigo}")
     buscador_carrefour(codigo)
     print("\n")
@@ -24,3 +26,4 @@ for codigo in codigos_barra:
     print("\n")
     scrape_farmacia_lider(codigo)
     print("\n")
+    buscador_superMami(codigo)
---
+++ SuperMami.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+from selenium.webdriver.firefox.options import Options
+
+def buscador_superMami(codigo_barras):
+    options = Options()
+    options.add_argument("--headless")
+    
+    driver = webdriver.Firefox(options=options)
+    
+    # Navegar a la página de Super Mami Argentina
+    driver.get("https://www.supermami.com.ar/super/home")
+
+    print("---------- Super Mami ----------")
+
+    boton = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "button.btn.btn-nobg.getFullSearch"))
+    )
+
+    # Hacer clic en el botón
+    boton.click()
+
+    # Pegar el código de barras directamente en el campo de búsqueda
+    campo_busqueda = driver.find_element(By.ID, "searchText")
+    campo_busqueda.send_keys(codigo_barras)
+    campo_busqueda.submit()
+
+    time.sleep(3)  
+    
+    try:
+        # Esperar a que se carguen los resultados de búsqueda
+        WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.precio-unidad"))
+        )
+
+        # Encontrar los elementos que contienen los precios del producto buscado
+        precios = driver.find_elements(By.CSS_SELECTOR, "div.precio-unidad span")
+
+        if len(precios) >= 2:
+            precio_actual = precios[0].text.strip()
+            precio_anterior = precios[1].text.strip()
+
+            print("Precio actual del producto buscado: " + precio_actual)
+            print("Precio anterior del producto buscado: " + precio_anterior)
+        else:
+            print("No se encontraron precios para el producto buscado.")
+
+    except:
+        print("Producto no encontrado")
+
+    # Cerrar el navegador
+    driver.quit()
+
