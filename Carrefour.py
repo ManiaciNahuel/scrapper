ea1a7efc2bc3a4fbec815b9139de5909ba3c275e
ManiaciNahuel <nahuelmaniaci@gmail.com>
2024-05-21 08:56:40 -0300

    Dataframe con carrefour ok

--- Carrefour.py
+++ Carrefour.py
@@ -9,78 +9,64 @@ from selenium.webdriver.common.keys import Keys
 import pandas as pd
 from selenium.webdriver.firefox.options import Options
 
-
 def buscador_carrefour(codigo_barras):
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
     
-    # Navegar a la página de Carrefour Argentina
+    salida = {"producto": "Producto", "precio_actual": 0, "precio_anterior": 0}
     driver.get("https://www.carrefour.com.ar/")
 
     print("---------- Carrefour ----------")
 
-    # Esperar a que el elemento de búsqueda esté presente y visible
-    search_box = WebDriverWait(driver, 1).until(
+    time.sleep(2)
+    search_box = WebDriverWait(driver, 5).until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, "input.vtex-styleguide-9-x-input.ma0.border-box.vtex-styleguide-9-x-hideDecorators.vtex-styleguide-9-x-noAppearance.br2.br-0.br--left.w-100.bn.outline-0.bg-base.c-on-base.b--muted-4.hover-b--muted-3.t-body.pl5"))
     )
 
     try:
         search_box.clear()
-        # Ingresar el código de barras
         search_box.send_keys(codigo_barras)
-        # Pequeña pausa antes de enviar la búsqueda
-        time.sleep(1)
-        
-        # Enviar la búsqueda presionando Enter
         search_box.send_keys(Keys.RETURN)
-        
-    except TimeoutException:
-        print("No tiene descuento")
+        time.sleep(5)
+    
     except StaleElementReferenceException:
-        #El elemento de búsqueda se ha vuelto 'stale', volviendo a buscarlo
         search_box = driver.find_element(By.CSS_SELECTOR, "input.vtex-styleguide-9-x-input.ma0.border-box.vtex-styleguide-9-x-hideDecorators.vtex-styleguide-9-x-noAppearance.br2.br-0.br--left.w-100.bn.outline-0.bg-base.c-on-base.b--muted-4.hover-b--muted-3.t-body.pl5")
         search_box.clear()
         search_box.send_keys(codigo_barras)
-        time.sleep(1)
-        
-        # Enviar la búsqueda presionando Enter
         search_box.send_keys(Keys.RETURN)
+        time.sleep(5)
 
-    # Esperar a que se cargue la página de resultados
-        
     driver.implicitly_wait(3)
 
     try:
-
-        # Esperar a que se cargue el precio del producto 
-        # Selling price es precio actual con descuento incluido si lo tiene
-        price_element = WebDriverWait(driver, 1).until(
+        price_element = WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located((By.CSS_SELECTOR, "span.valtech-carrefourar-product-price-0-x-sellingPrice"))
         )
 
         try:
-            # List price es precio sin descuento 
-            old_price_element = WebDriverWait(driver, 1).until(
+            old_price_element = WebDriverWait(driver, 5).until(
                 EC.visibility_of_element_located((By.CSS_SELECTOR, "span.valtech-carrefourar-product-price-0-x-listPrice"))
             )
             
-            # Extraer el precio completo utilizando el atributo textContent
-            price = price_element.get_attribute("textContent")
-            old_price = old_price_element.get_attribute("textContent")
-            
-            # Imprimir el precio del producto
-            print("Precio del producto actual:", price)
-            print("Precio del producto de lista:", old_price)
-            
+            if price_element:
+                price = price_element.get_attribute("textContent")
+                old_price = old_price_element.get_attribute("textContent")
+                print("Precio del producto actual:", price)
+                print("Precio del producto de lista:", old_price)
+                salida = {"producto": "Producto", "precio_actual": price, "precio_anterior": old_price}
+                
+                
         except TimeoutException:
-            # Si no se encuentra el precio sin descuento, imprimir solo el precio actual
             price = price_element.get_attribute("textContent")
             print("Precio del producto actual:", price)
             print("No tiene descuento")
+            salida = {"producto": "Producto", "precio_actual": price, "precio_anterior": price}
+            
+            
         
     except TimeoutException:
         print("Producto no encontrado")
-
-    # Cerrar el navegador
+        
+    return salida
     driver.quit()
--- Main.py
+++ Main.py
@@ -5,28 +5,36 @@ from Libertad import buscador_libertad
 from Lider import buscador_lider
 from SuperMami import buscador_superMami
 
+
 def leer_codigos_desde_excel(archivo_excel):
     try:
-        # Lee el archivo Excel y obtiene los códigos de barras de la primera columna
-        df = pd.read_excel(archivo_excel, header=None)  # Sin encabezados
-        codigos = df.iloc[:, 0].tolist()  # Toma solo la primera columna
-        return codigos
+        df = pd.read_excel(archivo_excel, header=None)
+        codigos = df.iloc[:, 0].tolist()
+        nombres_productos = df.iloc[:, 1].tolist()  # Leer la segunda columna con los nombres de los productos
+        return codigos, nombres_productos
     except Exception as e:
         print("Error al leer el archivo Excel:", e)
-        return []
+        return [], []
 
-archivo_excel = "codigos.xlsx"  # Ruta de tu archivo Excel
-codigos_barra = leer_codigos_desde_excel(archivo_excel)
+archivo_excel = "codigos.xlsx"
+codigos_barra, nombres_productos = leer_codigos_desde_excel(archivo_excel)
 
-for codigo in codigos_barra:
-    print("\n")
-    print(f"Buscando productos para el código de barras: {codigo}")
-    buscador_carrefour(codigo)
-    print("\n")
-    buscador_libertad(codigo)
-    print("\n")
-    buscador_lider(codigo)
-    print("\n")
-    buscador_superMami(codigo)
-    print("\n")
-    buscador_farmacity(codigo)
+# Crear un DataFrame vacío
+df_resultados = pd.DataFrame(columns=["Código de Barras", "Sitio", "Producto", "Precio Actual", "Precio Anterior"])
+
+print("---------- Cargando ----------")
+for codigo, nombre_producto in zip(codigos_barra, nombres_productos):
+    """ print("\n")
+    print(f"Buscando productos para el código de barras: {codigo}") """
+    
+    # Buscar en Carrefour
+    resultado_carrefour = buscador_carrefour(codigo)
+    if resultado_carrefour["producto"]:
+        df_resultados = pd.concat([df_resultados, pd.DataFrame([[codigo, nombre_producto, "Carrefour", resultado_carrefour["precio_actual"], resultado_carrefour["precio_anterior"]]], columns=df_resultados.columns)], ignore_index=True)
+    
+
+# Mostrar el DataFrame con los resultados
+print(df_resultados)
+
+# Guardar el DataFrame en un archivo CSV
+df_resultados.to_csv("resultados_busqueda.csv", index=False)
